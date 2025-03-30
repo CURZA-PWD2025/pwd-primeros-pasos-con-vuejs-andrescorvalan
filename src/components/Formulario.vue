c4183b890750a070c4e56619011d5600f4042fee
Andrés Corvalan <alcorvalan@gmail.com>
2025-03-30 18:45:54 -0300

    Version 1 del TP1

---
+++ src/components/Formulario.vue
@@ -0,0 +1,135 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg?: string }>()
+
+const nombre = ref('')
+const correo = ref('')
+const contrasenia = ref('')
+const fechanac = ref('')
+
+function limpiarFormulario() {
+  nombre.value = '';
+  correo.value = '';
+  contrasenia.value = '';
+  fechanac.value = '';
+}
+
+function crearUsuario() {
+  if (nombre.value && correo.value && fechanac.value && contrasenia.value){
+    const usuario = {
+      nombre: nombre.value,
+      correo: correo.value,
+      fechanac: fechanac.value,
+      contrasenia: contrasenia.value,
+      calcularEdad() {
+        const ahora = new Date();
+        const nacimiento = new Date(fechanac.value);
+        let edad = ahora.getFullYear() - nacimiento.getFullYear();
+        const mes = ahora.getMonth() - nacimiento.getMonth();
+        const dia = ahora.getDate() - nacimiento.getDate();
+        if (mes < 0 || (mes === 0 && dia < 0))
+          edad--;
+        return edad;
+      },
+      mostrarDatos() {
+        alert(
+          "Usuario creado correctamente"+
+          "\n Nombre: "+this.nombre +
+          "\n Correo electronico: "+this.correo +
+          "\n Contraseña: "+this.contrasenia +
+          "\n Fecha de nacimiento: "+this.fechanac+
+          "\n Edad: "+this.calcularEdad());
+        limpiarFormulario();
+      }
+    }
+    usuario.mostrarDatos();
+  }else 
+    alert("Los datos son obligatorios: ");
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <form class="formulario">
+    <fieldset>
+        <legend>Datos del usuario</legend> 
+        <label class="fieldForm">Nombre:
+          <abbr title="Ingrese su nombre (obligatorio)">*</abbr>
+          <input type="text" v-model="nombre"  placeholder="Debe ingresar su nombre" maxlength="20" pattern="[A-Za-z ]*" required>
+        </label>
+        <label class="fieldForm">Correo electr&oacute;nico:
+          <abbr title="Ingrese un email (usuario@dominio) (obligatorio)">*</abbr>
+          <input type="email" v-model="correo" placeholder="Debe ingresar un email" required>
+        </label>
+        <label class="fieldForm">Contraseña:
+          <abbr title="Ingrese una contraseña (obligatorio)">*</abbr>
+          <input type="password" v-model="contrasenia" placeholder="Ingrese su contraseña" required>
+        </label>
+        <label class="fieldForm">Fecha de nacimiento:
+          <abbr title="Ingrese su fecha de nacimiento (obligatorio)">*</abbr>
+          <input type="date" v-model="fechanac" placeholder="Ingrese su Fecha de Nacimiento" required>
+        </label>
+    </fieldset>
+  </form>
+  <!--Creo botones en vez de inputs submit y reset para usar los metodos:
+        * crear, que simula que se grabo bien y borra los campos
+        * limpiar. que limpia los campos
+      Porque con submit y reset se limpian automaticamente los campos-->
+  <button class="boton" @click="crearUsuario">Crear usuario</button>
+  <button class="boton" @click="limpiarFormulario">Limpiar formulario</button>
+  <div class="datosFormulario">
+    <h2>Datos ingresados</h2>
+    <p>Nombre: {{ nombre }}</p>
+    <p>Contraseña: {{ contrasenia }}</p>
+    <p>Correo electronico: {{ correo }}</p>
+    <p>Fecha de nacimiento: {{ fechanac }}</p>
+  </div>
+</template>
+
+<style scoped>
+* {
+    border: 0;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+} 
+form * {
+  display: block;
+  text-align: left;
+}
+fieldset {
+  border: 2px solid gray;
+  padding: 0.5em;
+}
+.fieldForm {
+  display: inline-block;
+  margin: 10px;
+}
+.fieldForm abbr {
+  display: inline-block;
+  color: red;
+	text-decoration: none;
+}
+input {
+  border: 1px solid green;
+  width: 100%;
+  height:2em;
+}
+.boton{
+  border-radius: 0.5em 0.5em 0.5em 0.5em;
+  border: 1px solid red;
+  display: inline-block;
+  margin: 0.5em;
+  padding: 0.2em 0.4em 0.2em 0.4em;
+  text-align: center;
+  width: auto;
+}
+.datosFormulario {
+  border: 1px solid green;
+}
+
+
+
+
+</style>
